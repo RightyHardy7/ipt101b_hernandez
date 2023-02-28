915c49b2fa0157930cf1e5118f42304bc3e287c8
hannahleiamba <amba.hannah_lei@hnu.edu.ph>
2023-02-28 16:44:32 +0800

    files

--- pullRequest_Hernandez.c
+++ pullRequest_Hernandez.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+
+// Programmer 2. This part must be used for the pull request.
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
 // Programmer 1. This part must be used for the main branch. 
 int main() {
     int num1=1, num2=2;
