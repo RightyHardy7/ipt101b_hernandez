2f1734790f6474ff48e35b53eb833ef1f4f1730a
RightyHardy7 <123623374+RightyHardy7@users.noreply.github.com>
2023-02-28 16:29:48 +0800

    Create pullRequest_Hernandez.c

---
+++ pullRequest_Hernandez.c
@@ -0,0 +1,8 @@
+// Programmer 1. This part must be used for the main branch. 
+int main() {
+    int num1=1, num2=2;
+    
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+    
+    return 0;
+}
