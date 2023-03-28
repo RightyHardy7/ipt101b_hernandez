fb7ac2fa8fe771f382911fcf9f65ba7e4b82f484
RightyHardy7 <123623374+RightyHardy7@users.noreply.github.com>
2023-03-28 16:15:24 +0800

    Add files via upload

---
+++ MultiplicationTable.php
@@ -0,0 +1,17 @@
+<hmtl>
+<head><title>MULTIPLICATION</title>
+</head>
+<body>
+    <center>
+    <h1>Multiplication Table of 5</h1>
+    <?php
+        $number = 5;
+
+    for($i=1; $i<=10; $i++){
+            echo $number ."x". $i ."=". $number * $i ."<br><br>";
+        // echo "Arthe";
+        }
+    ?>
+    </center>
+</body>
+</html>
